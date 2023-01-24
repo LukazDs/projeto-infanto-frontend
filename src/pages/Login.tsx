1cc7591b0245b90e383b42729a271ff933a9e7d3
LukazDs <lukazds2016@gmail.com>
2023-01-24 14:19:45 -0300

    feat: get values to inputs

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,21 +4,33 @@ import { Form } from '../components/form/styledForm'
 import { Logo } from '../components/logo/Logo'
 import { Password, SignIn } from "phosphor-react"
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 export function Login() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
     return (
         <Container>
             <Logo />
             <Form>
                 <Option>
                     <SignIn size={22}/>
-                    <Label>Email</Label>
-                    <Input />
+                    <Label id='email'>Email</Label>
+                    <Input 
+                        id='email' 
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)} 
+                    />
                 </Option>
                 <Option>
                     <Password size={22}/>
-                    <Label>Password</Label>
-                    <Input />
+                    <Label id='password'>Password</Label>
+                    <Input 
+                        id='password' 
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)} 
+                    />
                 </Option>
                 
                 <Button>Login</Button>
